e8a942d05f120c9edbdfc1432408256ad2f5c5ed
d4vc-198 <contactodiegoavc@gmail.com>
2023-03-22 14:58:51 -0600

    setup class added

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""test for database storage"""
+import unittest
+import pep8
+import json
+import os
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.db_storage import DBStorage
+from os import getenv
+
+
+class TestDBStorage(unittest.TestCase):
+    """ Test DBStorage """
+
+    @classmethod
+    def setUpClass(cls):
+        """set up for test"""
+        cls.user = User()
+        cls.user.first_name = "Kev"
+        cls.user.last_name = "Yo"
+        cls.user.email = "1234@yahoo.com"
+        cls.storage = DBStorage()
