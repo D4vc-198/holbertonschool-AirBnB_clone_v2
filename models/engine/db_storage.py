26cf9e5b790d77bfe8331723d441525be32409c1
d4vc-198 <contactodiegoavc@gmail.com>
2023-03-24 11:02:46 -0600

    save method added

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -19,3 +19,34 @@ class DBStorage():
                     getenv("HBNB_MYSQL_HOST"),
                     getenv("HBNB_MYSQL_DB")),
             pool_pre_ping=True)
+
+    def all(self, cls=None):
+        """returns all objects of cls"""
+        from models.state import State
+        from models.city import City
+        from models.user import User
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        class_list = [
+            State,
+            City,
+            User,
+            Place,
+            Review,
+            Amenity
+        ]
+        rows = []
+        if cls:
+            rows = self.__session.query(cls)
+        else:
+            for cls in class_list:
+                rows += self.__session.query(cls)
+        return {type(v).__name__ + "." + v.id: v for v in rows}
+
+    def new(self, obj):
+        """add object to db"""
+        if not obj:
+            return
+        self.__session.add(obj)
